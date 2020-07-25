ae42bbfd9f521af1990386f1eb81f633ad515274
Shashi Kant <56475085+Shashikant17@users.noreply.github.com>
2020-07-26 00:34:40 +0530

    commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h1>Shashi Kant</h1>
+<h3>Welcome to my page</h3>
+
+<p>Terraform integrated with AWS</p>
+<br>
+<?php
+  $cloudfront_url = `head -n1 myimg.txt`;
+  $img_path = "https://".$cloudfront_url."/awsefs.jpg";
+  echo "<br>";
+  echo "<img src='${img_path}' width=1280 height=720>";
+?>
+</body>
+</html>
